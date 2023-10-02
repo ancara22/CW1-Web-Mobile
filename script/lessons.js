d19f27bb0a7d08fbc9af34c0d7c3f94044e2e74d
Dionis Barcari <dionisbarcari@Dioniss-MacBook-Pro.local>
2023-10-02 23:26:37 +0100

    more comments

--- script/lessons.js
+++ script/lessons.js
@@ -1,56 +1,51 @@
 
-
+//Lessons page Vue application
 const lessons_app = new Vue({
     el: '#lessons_app',
     data: {
-        fetchedData: [],
-        searchedData: [],
-        shopingCart: [],
-        cartSize: 0,
-        sortBy: 'subject',
-        sortDirection: 'asc',
-        searchValue: '',
+        fetchedData: [],    //Fetched data from Json file
+        searchedData: [],   //Sorted data and filtered by search input. Render content
+        shopingCart: [],    //Shoping cart clist
+        cartSize: 0,        //Number of items in the cart
+        sortBy: 'subject',  //Sort by. subject-default
+        sortDirection: 'asc',   //Sort direction. ASC-default
+        searchValue: '',        //Searche input value
     },
 
     computed: {
+        //Sort the Rendered list
         sortedList: function() {
             return this.searchedData.sort((a, b) => {
+                //If sort by Price, convert data to Int
                 if(this.sortBy == 'price') {
                     a['price'] = parseInt(a['price'], 10);
                     b['price'] = parseInt(b['price'], 10);
                 }
 
+                //Set ASC or DESC
                 if(this.sortDirection == 'asc') {
-                    if(a[this.sortBy] > b[this.sortBy]) {
-                        return 1;
-                    } 
-                        return -1;
+                    return a[this.sortBy] > b[this.sortBy] ? 1 : -1;
                 } else if(this.sortDirection == 'desc') {
-                    if(a[this.sortBy] < b[this.sortBy]) {
-                        return 1;
-                    } 
-                        return -1;
+                    return a[this.sortBy] < b[this.sortBy] ? 1: -1;
                 }
-
            })
-
-        },
-
-        
+        } 
     },
 
     created: function() {
+        //Fetch the data from JSON file on page open
         this.fetchData();
-        console.log('first', this.fetchedData)
-        console.log('first', this.searchedData)
     },
 
     methods: {
+        //Event handler, on click go to Cart page
         goToCart: function() {
             window.location = '../html/cart.html';
         },
 
+        //Event handler, on button click add the item to shopping cart
         addToCart: function(id) {
+            //Find the lesson by ID and update the spaces
             let item = this.fetchedData.filter(lesson => { 
                     if(lesson.id == id && lesson.spaces > 0)  {
                         lesson.spaces--;
@@ -58,60 +53,68 @@ const lessons_app = new Vue({
                     }
             });
 
+            //Add the item to shopping Cart Array, update the cart size
             if(item.length > 0) {
                 this.shopingCart.push(...item);
                 this.cartSize++;
+
+                //Update the local store
                 localStorage.setItem('cartItems', JSON.stringify(this.shopingCart));
             }
 
 
+            //Get data from localstore
             const lessonsStoreLocal = localStorage.getItem('lessonsStoreLocal');
 
-            if(lessonsStoreLocal != null) {
+            if(localStorage.getItem('lessonsStoreLocal') != null) {
                 localStorage.setItem('lessonsStoreLocal', JSON.stringify(this.fetchedData));
-            } 
-
-                
+            }   
         },
 
+        //Check if the lesson has more spaces
         isDisable: function(spaces) {
             return spaces == 0;
         },
 
+        //Search the lessons by input value
         searchLesson: function() {
             this.searchedData = this.fetchedData.filter(lesson => {
                 let keysearch =  lesson.subject + ' ' + lesson.location;
                 
+                //Check if the search input value is included in lesson info
                 if(this.searchValue != '') {
                     if(keysearch.toLocaleLowerCase().includes(this.searchValue.toLocaleLowerCase())) {
                         return lesson;
-                    }
-                       
+                    }   
                 } else {
                     return lesson;
                 }
            }) 
         },
 
+        //Fetch data from JSOn file function
         fetchData: function() {
             fetch('../lessons.json')
                 .then(response => response.json())
                 .then(data => {
+                    //Get data from local store
                     const lessonsStoreLocal = localStorage.getItem('lessonsStoreLocal');
             
+                    //Save data to array and search, to update the rendered list
                     if(lessonsStoreLocal == null) {
                         localStorage.setItem('lessonsStoreLocal', JSON.stringify(data));
                         this.fetchedData = data;
-                        this.searchLesson();
+                        this.searchLesson(); //Update the rendering list
                     } else {
                         this.fetchedData = JSON.parse(lessonsStoreLocal);
-                        this.searchLesson();
+                        this.searchLesson(); //Update the rendering list
                     }
 
                 }).catch(error => {
                     console.error('Error loading data:', error);
                 });
 
+                //Save Cart Items in local Store
                 const cartLocalStore = localStorage.getItem('cartItems');
 
                 if(cartLocalStore != null) {
